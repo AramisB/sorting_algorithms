acb1574c9e03b260bc8f5378973764c93ac2e1b5
AramisB <brendachem7@gmail.com>
2024-02-16 12:39:32 +0300

    bubblesort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,37 +1,7 @@
 #include <stdio.h>
 #include "sort.h"
+#include <stdbool.h>
 void swap(int *x, int *y);
-
-/**
- * bubble_sort - a function that sorts an array of integers 
- * in ascending order using the Bubble sort algorithm
- *
- * @array: the array to be printed
- * @size: size of the array
- *
- */
-void bubble_sort(int *array, size_t size)
-{
-	size_t i, j;
-	int swapped;
-
-	for (i = 0; i < (size - 1); i++)
-	{
-		swapped = 0;
-
-		for (j = 0; j < (size - i - 1); j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				swap (&array[j], &array[j + 1]);
-				swapped = 1;
-				print_array(array, size);
-			}
-		}
-		if (!swapped)
-			break;
-	}
-}
 
 /**
  * swap - swaps x and y
@@ -44,4 +14,37 @@ void swap(int *x, int *y)
 	int temp = *x;
 	*x = *y;
 	*y = temp;
+}
+/**
+ * bubble_sort - a function that sorts an array of integers
+ * in ascending order using the Bubble sort algorithm
+ *
+ * @array: the array to be printed
+ * @size: size of the array
+ *
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, len = size;
+	bool swapped = false;
+
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+
+	while (swapped == false)
+	{
+		swapped = true;
+		for (i = 0; i < (len - 1); i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap(&array[i], &array[i + 1]);
+				print_array(array, size);
+				swapped = false;
+			}
+		}
+		len--;
+	}
 }
