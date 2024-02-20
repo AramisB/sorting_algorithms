a48523e4e041692c062106241ad841140901445d
AramisB <brendachem7@gmail.com>
2024-02-20 12:07:33 +0300

    coctail shake sort algorithm

--- sort.h
+++ sort.h
@@ -16,6 +16,18 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
+
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void swap(int *x, int *y);
@@ -24,5 +36,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 #endif
